a04c96b97fadf93ca69f73c3e56e7dca0d013f71
shq-0103 <shq0103@gmail.com>
2020-11-27 14:28:22 +0800

    组件吸顶

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,8 +1,32 @@
 <template>
-  <div>product</div>
+  <div class="product">
+    <product-param
+      ><template v-slot:buy>
+        <button class="btn">立即购买</button></template
+      ></product-param
+    >
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+  </div>
 </template>
 <script>
-export default {
-  name: "product",
-};
+import ProductParam from "./../components/ProductParam";
+export default { name: "product", components: { ProductParam } };
 </script>
+<style lang="scss">
+.product {
+  button {
+    margin-left: 10px;
+  }
+  .h {
+    height: 150px;
+  }
+}
+</style>
